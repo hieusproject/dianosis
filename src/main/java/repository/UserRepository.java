37deb323e417f165431233e100c3de77661e3876
annguyen0505 <ancm.edu@gmail.com>
2017-10-05 15:58:24 +0700

    just practice push command

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -37,5 +37,26 @@ public class UserRepository {
     return users;
     
     }
+     public User getUserByInput(String username,String password){
+     try {
+      
+           String sqlString= "SELECT * FROM `usertb` WHERE _username= ? and _password=?";
+           PreparedStatement getStatement= connection.prepareStatement(sqlString);
+                        getStatement.setString(1,username);
+                        getStatement.setString(2,password);
+           ResultSet rs= getStatement.executeQuery();
+           
+             while (rs.next()) {                 
+                 User user= new User(rs.getInt("u_id"), rs.getString("_username"),
+                         rs.getString("_password"), rs.getString("fullName"),
+                         rs.getString("address"), rs.getString("phone"),
+                         rs.getString("email"), rs.getString("birthdate"), rs.getInt("role"));
+                 return user;
+             }
+       } catch (Exception e) {
+       }
+    return null;
+    
+    }
     
 }
