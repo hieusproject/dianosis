e6f8e35d5b7ced1e57346280349119da3ecf188d
annguyen0505 <ancm.edu@gmail.com>
2017-10-13 16:47:51 +0700

    update 3 funtions of Solution,complicated query statement

--- src/main/java/controller/SolutionController.java
+++ src/main/java/controller/SolutionController.java
@@ -5,7 +5,16 @@
  */
 package controller;
 
+import entity.Token;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import repository.SolutionRepository;
+import repository.TokenRepository;
 
 /**
  *
@@ -13,6 +22,59 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class SolutionController {
+    private static SolutionRepository solutionRepository= new SolutionRepository();
+    private static TokenRepository tokenRepostirory= new TokenRepository();
+    @RequestMapping(value = "/get_recommended_solution",method=RequestMethod.GET)
+    public Map getRecommededSolution(@RequestParam(name="token") String token,
+                                     @RequestParam(name="c_id") String c_idStr){
+        Map respone= new HashMap();
+        int c_id=Integer.parseInt(c_idStr);
+        Token tokenOb = tokenRepostirory.getTokenByCode(token);
+        if (token==null) {
+            respone.put("status","0"); 
+        }
+        else{
+            respone.put("status","1");
+            ArrayList<Map> solutionList= solutionRepository.getTopByc_id(c_id);
+            respone.put("solutions",solutionList);
+            
+        }
+        return respone;
+    }
+    @RequestMapping(value = "/get_top_solution",method=RequestMethod.GET)
+     public Map getTopbyLikesSolution(@RequestParam(name="token") String token,
+                                     @RequestParam(name="page") String page){
+        Map respone= new HashMap();
+        int pageIndex=Integer.parseInt(page);
+        Token tokenOb = tokenRepostirory.getTokenByCode(token);
+        if (token==null) {
+            respone.put("status","0"); 
+        }
+        else{
+            respone.put("status","1");
+            ArrayList<Map> solutionList= solutionRepository.getTopbyPage(pageIndex);
+            respone.put("solutions",solutionList);
+            
+        }
+        return respone;
+    }
+     @RequestMapping(value = "/get_user_solution",method=RequestMethod.GET)
+    public Map getRatedSolutionByC_id(@RequestParam(name="token") String token,
+                                     @RequestParam(name="c_id") String c_idStr){
+        Map respone= new HashMap();
+        int c_id=Integer.parseInt(c_idStr);
+        Token tokenOb = tokenRepostirory.getTokenByCode(token);
+        if (token==null) {
+            respone.put("status","0"); 
+        }
+        else{
+            respone.put("status","1");
+            ArrayList<Map> solutionList= solutionRepository.getRatedSolution(c_id);
+            respone.put("solutions",solutionList);
+            
+        }
+        return respone;
+    }
     
     
 }
--- src/main/java/repository/SolutionRepository.java
+++ src/main/java/repository/SolutionRepository.java
@@ -42,26 +42,91 @@ public class SolutionRepository implements  RepositoryInterface{
      public ArrayList<Map> getTopByc_id(int c_id) {
         ArrayList<Map> result= new ArrayList<Map>();
         try {
-            String getSQL="SELECT slt.s_title as title,slt.s_content as content ,likeview.likes as likes"
+            String getSQL="SELECT slt.s_id as s_id,slt.s_title as title,slt.s_content as content ,likeview.likes as likes"
                     + " FROM solution slt JOIN child_solution_recommend c_slt_rcm ON slt.s_id=c_slt_rcm.s_id  "
                     + "Join likeview ON slt.s_id= likeview.s_id "
                     + "WHERE c_id=? ORDER BY c_slt_rcm.rating DESC LIMIT 5";
             PreparedStatement getST= connection.prepareStatement(getSQL);
             getST.setInt(1, c_id);
             ResultSet rs=getST.executeQuery();
+             System.out.println("get sucsess");
             while (rs.next()) {     
                 Map line= new HashMap();
                 line.put("title",rs.getString("title"));
                 line.put("content",rs.getString("content"));
                 line.put("likes",rs.getInt("likes"));
-                result.add(line);
+                Map solution= new HashMap();
+                solution.put(Integer.toString(rs.getInt("s_id")), line);
+                result.add(solution);
             }
             
         } catch (Exception e) {
+             e.printStackTrace();
         }
         return result;
     }
-    
+      public ArrayList<Map> getTopbyPage(int pageIndex) {
+         int begin= (pageIndex-1)*10;
+         int end=pageIndex*10-1;
+        ArrayList<Map> result= new ArrayList<Map>();
+        
+        try {
+            String getSQL="SELECT solution.s_id as s_id,s_title,s_content,likeview.likes as likes"
+                    + " FROM `solution` JOIN likeview  ON solution.s_id=likeview.s_id "
+                    + " GROUP BY solution.s_id "
+                    + " ORDER BY likes DESC  "
+                    + " LIMIT ?,? ";
+            PreparedStatement getST= connection.prepareStatement(getSQL);
+            getST.setInt(1, begin);
+            getST.setInt(2, end);
+            ResultSet rs=getST.executeQuery();
+             System.out.println("get sucsess");
+            while (rs.next()) {     
+                Map line= new HashMap();
+                line.put("title",rs.getString("title"));
+                line.put("content",rs.getString("content"));
+                line.put("likes",rs.getInt("likes"));
+                Map solution= new HashMap();
+                solution.put(Integer.toString(rs.getInt("s_id")), line);
+                result.add(solution);
+            }
+            
+        } catch (Exception e) {
+             e.printStackTrace();
+        }
+        return result;
+    }
+       public ArrayList<Map> getRatedSolution(int c_id) {
+         
+        ArrayList<Map> result= new ArrayList<Map>();
+        
+        try {
+            String getSQL="SELECT solution.s_id as s_id,s_title,s_content,likeview.likes as likes"
+                    + " FROM solution JOIN child_solution"
+                    + " ON solution.s_id= child_solution.s_id "
+                    + "JOIN likeview "
+                    + "ON solution.s_id=likeview.s_id  "
+                    + " WHERE child_solution.c_id=  ? ";
+            PreparedStatement getST= connection.prepareStatement(getSQL);
+            getST.setInt(1, c_id);
+            ResultSet rs=getST.executeQuery();
+             System.out.println("get sucsess");
+            while (rs.next()) {  
+            
+                Map line= new HashMap();
+                line.put("title",rs.getString("title"));
+                line.put("content",rs.getString("content"));
+                line.put("likes",rs.getInt("likes"));
+                Map solution= new HashMap();
+                solution.put(Integer.toString(rs.getInt("s_id")), line);
+                result.add(solution);
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
     @Override
      public boolean save(Object ob) {
         try {
@@ -122,5 +187,10 @@ public class SolutionRepository implements  RepositoryInterface{
     @Override
     public boolean deleteById(String id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public static void main(String[] args) {
+        SolutionRepository soRepository= new SolutionRepository();
+        soRepository.getTopbyPage(1);
     }
 }
